82553f6292c206f348d7659da27ce644b376c8d3
slubendo <stephane.lubendo@gmail.com>
2023-03-26 11:25:05 -0700

    first commit

--- StephaneTDD.js
+++ StephaneTDD.js
@@ -23,3 +23,9 @@ const validate2 = (username) => {
 console.log("return true if username if not empty", validate1(""))
 console.log("return true if username if not empty", validate1("Stephane123"))
 console.log("return true if username is already in database", validate2("Stephane123"))
+
+//Brett Comment: the test driven test checks for username inside database. Simple but good at this stage.
+
+// Royce Comment:the database could have more information in it but good test for what were trying to achieve.
+
+// Alice Comment: Good test ensures that there are no duplicate usernames in the database.
