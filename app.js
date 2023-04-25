ee86496c0d7c997efbb59131130fba82fa50f975
Royce Coviello <coviello.royce@gmail.com>
2023-04-25 14:02:32 -0700

    small fixes/cleanup added bot responses logging to mock db

--- app.js
+++ app.js
@@ -27,41 +27,47 @@ io.on("connection", (socket) => {
   const actions = {
     "@bot": async (msg) => {
       const prompt = msg.replace("@bot", "").trim();
-  
+
       try {
         io.emit("chat message", { username: socket.username, message: msg });
 
         const response = await promptMessage({ message: prompt, type: "chat" });
-        
+
         io.emit("chat message", { username: "ChatGPT", message: response });
+
+        chats.push({ username: socket.username, message: msg });
+        chats.push({ username: "ChatGPT", message: response });
       } catch (error) {
         console.error(error);
       }
     },
     "@help": async (msg) => {
-      io.emit("chat message", { username: "helper", message: "type @bot to prompt the bot" });
-    }
+      io.emit("chat message", {
+        username: "helper",
+        message: "type @bot to prompt the bot" + "\n" + "type @help for help",
+      });
+    },
 
     // Add more actions here as needed
   };
-  
+
   socket.on("chat message", async (msg) => {
     console.log(`message: ${msg}`);
-  
+
     for (const keyword in actions) {
       if (msg.includes(keyword)) {
         const action = actions[keyword];
         await action(msg);
+        // chats.push({ username: socket.username, message: msg });
         return; // Exit the loop after the first match is found
       }
     }
-  
+
     // Add the new chat to the mock database
     chats.push({ username: socket.username, message: msg });
     io.emit("chat message", { username: socket.username, message: msg }); // Send the message to all clients
     console.log(chats);
   });
-  
 
   socket.on("login", (data) => {
     console.log(`login: ${data.username}`);
@@ -81,17 +87,5 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log(`listening on port:${PORT}`);
 });
-
-// if (msg.includes("@test")) {
-//   const prompt = msg.replace("@test", "").trim();
-
-//   try {
-//     const response = "test received" + prompt;
-//     io.emit("chat message", { username: socket.username, message: prompt });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
 
 // todo pass last x messages to prompt for context
