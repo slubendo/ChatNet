1565a0a10793fbf1b4f730ba0384e7a35129d054
AliceEHZz <meiyellmya@gmail.com>
2023-05-02 15:33:58 -0700

    naming update

--- public/chatroomFrontend.js
+++ public/chatroomFrontend.js
@@ -14,29 +14,27 @@ document.querySelector(".sendIcon").addEventListener("click", function (e) {
   document.getElementById("m").value = "";
 });
 
-
 // Listen for the "chats" event and update the UI
-socket.on("chats", async function (chats, users, chatRoomId) {
-  let userName = await session()
-  console.log(userName)
-  console.log(users)
+socket.on("chats", async function (messages, usernames, chatRoomId) {
+  let userName = await session();
+  console.log(userName);
+  console.log(usernames);
   const messagesList = document.getElementById("messages");
-  for (let i = 0; i < chats.length; i++) {
-    if(chats[i].chatId == chatRoomId) {
+  for (let i = 0; i < messages.length; i++) {
+    if (messages[i].chatId == chatRoomId) {
       const li = document.createElement("li");
       const span = document.createElement("span"); // Create a <span> element to hold the username
-  
-      if(users[i] == userName) {
+
+      if (usernames[i] == userName) {
         li.classList.add("you");
-      } else if (users[i] == "ChatGPT") {
+      } else if (usernames[i] == "ChatGPT") {
         li.classList.add("chatGPT");
       }
-  
-      span.textContent = users[i] + ": "; // Set the text content of the <span> element to the username
-      li.appendChild(span); // Append the <span> element to the <li> element
-      li.textContent += chats[i].text; // Append the message to the <li> element
-      messagesList.appendChild(li);
 
+      span.textContent = usernames[i] + ": "; // Set the text content of the <span> element to the username
+      li.appendChild(span); // Append the <span> element to the <li> element
+      li.textContent += messages[i].text; // Append the message to the <li> element
+      messagesList.appendChild(li);
     }
   }
 });
@@ -45,24 +43,27 @@ socket.on("chat message", async function (data) {
   let userName = await session();
   const li = document.createElement("li");
 
-  if(data.username == userName) {
+  if (data.username == userName) {
     li.classList.add("you");
   } else if (data.username == "ChatGPT") {
     li.classList.add("chatGPT");
   }
-  const span = document.createElement("span") // Create a <span> element to hold the username
+  const span = document.createElement("span"); // Create a <span> element to hold the username
   span.textContent = data.username + ": "; // Set the text content of the <span> element to the username
   li.appendChild(span); // Append the <span> element to the <li> element
   li.textContent += data.message; // Append the message to the <li> element
   document.getElementById("messages").appendChild(li);
 });
 
-
 async function session() {
-  const response = await fetch(`/session`, { method: "GET", body: JSON.stringify(), headers: { "Content-Type": "application/json" } });
+  const response = await fetch(`/session`, {
+    method: "GET",
+    body: JSON.stringify(),
+    headers: { "Content-Type": "application/json" },
+  });
   const body = await response.json();
-  const user = body.session;
-    console.log(user)
+  const username = body.session;
+  console.log(username);
 
-  return user
+  return username;
 }
