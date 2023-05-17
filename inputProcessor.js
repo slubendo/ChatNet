5bf248bc6f4848d3078f09b3cb92968e20a40758
r <coviello.royce@gmail.com>
2023-05-17 00:14:31 -0700

    optimized code

--- inputProcessor.js
+++ inputProcessor.js
@@ -11,117 +11,92 @@ export function processInput(
 ) {
   input = input.trim();
 
-  if (input.startsWith("@")) {
-    const regex = /^@(\w+)(?:\((.*?)\))?((?:\s+-\w+(?:\([^)]*\))?)*)(?:\s+(.*))?$/;
-    const [, keyword, keywordParam, flagMatches, content] = regex.exec(input) || [];
-  
-    const flags = [];
-    const flagRegex = /(-\w+)(?:\((.*?)\))?/g;
-    let match;
-    while ((match = flagRegex.exec(flagMatches)) !== null) {
-      const [, flag, flagParam] = match;
-      const flagObj = { flag: flag.substring(1) };
-      if (flagParam) {
-        flagObj.parameter = flagParam;
-      }
-      flags.push(flagObj);
+  if (!input.startsWith("@")) {
+    // console.log('Input does not start with "@"');
+    return;
+  }
+
+  const regex =
+    /^@(\w+)(?:\((.*?)\))?((?:\s+-\w+(?:\([^)]*\))?)*)(?:\s+(.*))?$/;
+  const [, keyword, keywordParam, flagMatches, content] = regex.exec(input) || [];
+
+  const flags = [];
+  const flagRegex = /(-\w+)(?:\((.*?)\))?/g;
+  let match;
+  while ((match = flagRegex.exec(flagMatches)) !== null) {
+    const [, flag, flagParam] = match;
+    const flagObj = { flag: flag.substring(1) };
+    if (flagParam) {
+      flagObj.parameter = flagParam;
     }
-  
-    console.log("\n");
-    console.log("keyword:", keyword);
-    console.log("keywordParam:", keywordParam);
-    console.log("flags:", flags);
-    console.log("content:", content);
-  
-  
-  
+    flags.push(flagObj);
+  }
 
+  console.log("\n");
+  console.log("keyword:", keyword);
+  console.log("keywordParam:", keywordParam);
+  console.log("flags:", flags);
+  console.log("content:", content);
 
-    if (keyword) {
-      const lowercaseKeyword = keyword.toLowerCase();
-      const handler = keywordHandlers[lowercaseKeyword];
+  if (!keyword) {
+    // console.log("Invalid input format.");
+    return;
+  }
 
-      if (handler) {
-        if (typeof handler === "function") {
-          handler(
-            input,
-            socket,
-            io,
-            currentUser,
-            chatRoomId,
-            formattedAllChatMsg,
-            allChatMsg,
-            keywordParam
-          );
-        } else if (flag) {
-          const flagWithoutParam = flag.slice(1); // Remove the leading "-"
-          const flagHandler = handler[flagWithoutParam];
-          if (flagHandler && typeof flagHandler === "function") {
-            flagHandler(
-              input,
-              socket,
-              io,
-              currentUser,
-              chatRoomId,
-              formattedAllChatMsg,
-              allChatMsg,
-              flagParam
-            );
-          } else {
-            // console.log("Invalid flag.");
-          }
-        } else {
-          const defaultHandler = handler.default;
-          if (defaultHandler && typeof defaultHandler === "function") {
-            defaultHandler(
-              input,
-              socket,
-              io,
-              currentUser,
-              chatRoomId,
-              formattedAllChatMsg,
-              allChatMsg,
-              keywordParam
-            );
-          } else {
-            // console.log("Invalid keyword.");
-          }
-        }
+  const lowercaseKeyword = keyword.toLowerCase();
+  const handler = keywordHandlers[lowercaseKeyword];
+
+  if (!handler) {
+    // console.log("Invalid keyword.");
+    return;
+  }
+
+  if (typeof handler === "function") {
+    handler(
+      input,
+      socket,
+      io,
+      currentUser,
+      chatRoomId,
+      formattedAllChatMsg,
+      allChatMsg,
+      keywordParam
+    );
+  } else {
+    const flag = flags[0];
+    if (flag) {
+      const flagWithoutParam = flag.flag; // Remove the leading "-"
+      const flagHandler = handler[flagWithoutParam];
+      if (flagHandler && typeof flagHandler === "function") {
+        flagHandler(
+          input,
+          socket,
+          io,
+          currentUser,
+          chatRoomId,
+          formattedAllChatMsg,
+          allChatMsg,
+          flag.parameter
+        );
+      } else {
+        // console.log("Invalid flag.");
+      }
+    } else {
+      const defaultHandler = handler.default;
+      if (defaultHandler && typeof defaultHandler === "function") {
+        defaultHandler(
+          input,
+          socket,
+          io,
+          currentUser,
+          chatRoomId,
+          formattedAllChatMsg,
+          allChatMsg,
+          keywordParam
+        );
       } else {
         // console.log("Invalid keyword.");
       }
-    } else {
-      // console.log("Invalid input format.");
     }
-  } else {
-    // console.log('Input does not start with "@"');
   }
 }
-
-
-
-
-
-
-
-// input = input.trim();
-
-//   if (input.startsWith("@")) {
-//     const [, keyword, keywordParam, flagsAndParams, content] =
-//       /^@(\w+)(?:\((.*?)\))?(?:\s+((?:-\w+\s*\([^)]*\)\s*)*))?(?:\s+(.*))?$/.exec(
-//         input
-//       ) || [];
-//     console.log("\n");
-//     console.log("keyword:", keyword);
-//     console.log("keywordParam:", keywordParam);
-
-//     const flags = {};
-//     const flagRegex = /(-\w+)\s*\(([^)]*)\)/g;
-//     let flagMatches;
-//     while ((flagMatches = flagRegex.exec(flagsAndParams)) !== null) {
-//       const [, flag, flagParam] = flagMatches;
-//       flags[flag.slice(1)] = flagParam;
-//     }
-//     console.log("flags:", flags);
-
-//     console.log("content:", content);
