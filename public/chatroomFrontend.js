8deac7f0ee7a8ada20deb4f6361f9bccbdf4bfda
slubendo <stephane.lubendo@gmail.com>
2023-05-24 15:03:20 -0700

    copy code new

--- public/chatroomFrontend.js
+++ public/chatroomFrontend.js
@@ -1,17 +1,3 @@
-// import MarkdownIt from "markdown-it";
-// import hljs from "highlight.js";
-
-// const md = new MarkdownIt({
-//   highlight: function (str, lang) {
-//     return (
-//       '<pre class="hljs"><code>' +
-//       hljs.highlightAuto(str).value +
-//       "</code></pre>"
-//     );
-//   },
-//   html: true,
-// });
-
 const currentURL = window.location.href;
 const urlParts = currentURL.split("/");
 const chatRoomId = urlParts[urlParts.length - 1];
@@ -102,111 +88,128 @@ async function getCurrentUser() {
 
       outerDiv.prepend(messageDiv);
       messagesList.prepend(outerDiv);
+      document.addEventListener("click", async function (event) {
+        const copyButtonLabel = "Copy";
+        let preDiv = event.target.parentNode;
+        let pre = preDiv.parentNode;
+  
+        if (event.target.className.includes("copy")) {
+          let button = event.target
+          let code = pre.querySelector("code");
+          let text = code.innerText;
+          await navigator.clipboard.writeText(text)
+  
+            button.innerText = "Copied";
+            setTimeout(() => {
+              button.innerText = copyButtonLabel;
+            }, 700);
+        }
+      });
     }
   });
 
-  // FIX THIS !!!
-  socket.on("chat message", async function (data) {
-    const outerDiv = document.createElement("div");
-    const messageDiv = document.createElement("div"); // Create a <span> element to hold the username
-    outerDiv.classList.add("outer", "flex", "justify-start", "mb-4");
-    messageDiv.classList.add(
-      "message",
-      "mr-2",
-      "py-3",
-      "px-4",
+// FIX THIS !!!
+socket.on("chat message", async function (data) {
+  const outerDiv = document.createElement("div");
+  const messageDiv = document.createElement("div"); // Create a <span> element to hold the username
+  outerDiv.classList.add("outer", "flex", "justify-start", "mb-4");
+  messageDiv.classList.add(
+    "message",
+    "mr-2",
+    "py-3",
+    "px-4",
+    "bg-gray-400",
+    "rounded-br-3xl",
+    "rounded-tr-3xl",
+    "rounded-tl-xl",
+    "text-white"
+  );
+
+  messageDiv.innerHTML = data.username + ": ";
+  messageDiv.innerHTML += `<p>${data.message}</p>`;
+
+  if (currentUserData.username == data.username) {
+    outerDiv.classList.remove("justify-start");
+    outerDiv.classList.add("you", "justify-end");
+    messageDiv.classList.remove(
       "bg-gray-400",
       "rounded-br-3xl",
       "rounded-tr-3xl",
-      "rounded-tl-xl",
-      "text-white"
+      "rounded-tl-xl"
     );
+    messageDiv.classList.add(
+      "bg-blue-400",
+      "rounded-bl-3xl",
+      "rounded-tl-3xl",
+      "rounded-tr-xl"
+    );
+  } else if (data.username == "ChatGPT") {
+    console.log("data.username == ChatGPT");
 
-    messageDiv.innerHTML = data.username + ": ";
-    messageDiv.innerHTML += `<p>${data.message}</p>`;
+    messageDiv.classList.remove("bg-gray-400");
+    messageDiv.classList.add("chatGPT", "bg-green-500");
+  } else if (data.username == "System") {
+    console.log("data.username == System");
 
-    if (currentUserData.username == data.username) {
-      outerDiv.classList.remove("justify-start");
-      outerDiv.classList.add("you", "justify-end");
-      messageDiv.classList.remove(
-        "bg-gray-400",
-        "rounded-br-3xl",
-        "rounded-tr-3xl",
-        "rounded-tl-xl"
-      );
-      messageDiv.classList.add(
-        "bg-blue-400",
-        "rounded-bl-3xl",
-        "rounded-tl-3xl",
-        "rounded-tr-xl"
-      );
-    } else if (data.username == "ChatGPT") {
-      console.log("data.username == ChatGPT");
+    messageDiv.classList.remove("bg-gray-400");
+    messageDiv.classList.add("System", "bg-yellow-500");
+  }
 
-      messageDiv.classList.remove("bg-gray-400");
-      messageDiv.classList.add("chatGPT", "bg-green-500");
-    } else if (data.username == "System") {
-      console.log("data.username == System");
+  outerDiv.prepend(messageDiv);
 
-  messageDiv.classList.remove("bg-gray-400");
-  messageDiv.classList.add("System", "bg-yellow-500");
-}
+  if (chatRoomId == data.chatRoomId) {
+    document.getElementById("messages").prepend(outerDiv);
+  }
+});
 
-    outerDiv.prepend(messageDiv);
+const scrollingElement = document.getElementById("messages");
 
-    if (chatRoomId == data.chatRoomId) {
-      document.getElementById("messages").prepend(outerDiv);
+const config = { childList: true };
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      window.scrollTo(0, document.body.scrollHeight);
     }
-  });
+  }
+};
 
-  const scrollingElement = document.getElementById("messages");
+const observer = new MutationObserver(callback);
+observer.observe(scrollingElement, config);
 
-  const config = { childList: true };
-  const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        window.scrollTo(0, document.body.scrollHeight);
-      }
-    }
-  };
+//@ search user in database
+const addMemberMessage = document.getElementById("addMemberMessage");
+const emailInput = document.getElementById("simple-search");
+const addChatMemberBtn = document.getElementById("add-to-chat");
+const addMemberBackToChatBtn = document.getElementById(
+  "addMember-back-to-chat"
+);
 
-  const observer = new MutationObserver(callback);
-  observer.observe(scrollingElement, config);
+document
+  .getElementById("submitSearchInput")
+  .addEventListener("click", searchByEmail);
 
-  //@ search user in database
-  const addMemberMessage = document.getElementById("addMemberMessage");
-  const emailInput = document.getElementById("simple-search");
-  const addChatMemberBtn = document.getElementById("add-to-chat");
-  const addMemberBackToChatBtn = document.getElementById(
-    "addMember-back-to-chat"
-  );
+async function searchByEmail(event) {
+  event.preventDefault();
+  addMemberMessage.textContent = "";
+  addChatMemberBtn.classList.remove("noDisplay");
+  addMemberBackToChatBtn.classList.add("noDisplay");
+  const emailInputValue = emailInput.value;
 
-  document
-    .getElementById("submitSearchInput")
-    .addEventListener("click", searchByEmail);
+  try {
+    const response = await fetch("/search-email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ emailInputValue }),
+    });
 
-  async function searchByEmail(event) {
-    event.preventDefault();
-    addMemberMessage.textContent = "";
-    addChatMemberBtn.classList.remove("noDisplay");
-    addMemberBackToChatBtn.classList.add("noDisplay");
-    const emailInputValue = emailInput.value;
+    const result = await response.json();
 
-    try {
-      const response = await fetch("/search-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ emailInputValue }),
-      });
-
-      const result = await response.json();
-
-      if (result.success) {
-        // Display the result to the user
-        const { username, email } = result.resultedUser;
-        document.getElementById("result-container").innerHTML = `<h4
+    if (result.success) {
+      // Display the result to the user
+      const { username, email } = result.resultedUser;
+      document.getElementById("result-container").innerHTML = `<h4
         class="text-lg font-light text-gray-900 dark:text-white mb-1">
         Search results: </h4>
         <div
@@ -225,90 +228,26 @@ async function getCurrentUser() {
         
         </div>
         </div>`;
-      } else {
-        addMemberMessage.textContent = `No user found with email ${emailInputValue}`;
-      }
-    } catch (error) {
-      console.error("Error searching user:", error);
-    }
-  }
-
-  //@ add member to chat
-
-  document.getElementById("add-to-chat").addEventListener("click", addMember);
-  const resultContainer = document.getElementById("result-container");
-  async function addMember(event) {
-    event.preventDefault();
-    const checkbox = document.getElementById("default-checkbox");
-
-    if (checkbox.checked) {
-      const email = checkbox.value;
-      try {
-        const response = await fetch("/add-member", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, chatRoomId }),
-        });
-
-        const result = await response.json();
-
-        if (result.success) {
-          addMemberMessage.textContent = `${result.message}`;
-          addMemberMessage.classList.add("text-blue-500");
-          addMemberMessage.classList.remove("text-red-600");
-          addMemberBackToChatBtn.classList.remove("noDisplay");
-          addChatMemberBtn.classList.add("noDisplay");
-        } else {
-          console.log("error: ", result.error);
-          addMemberMessage.textContent = `${result.error}`;
-        }
-      } catch (error) {
-        addMemberMessage.textContent = `${error}`;
-      }
     } else {
-      addMemberMessage.textContent = `Please confirm the user by checking the box.`;
+      addMemberMessage.textContent = `No user found with email ${emailInputValue}`;
     }
+  } catch (error) {
+    console.error("Error searching user:", error);
   }
+}
 
-  //@ helper function: go back to chatroom
+//@ add member to chat
 
-  const backToChatroom = (triggeredBtn) => {
-    const roomId = triggeredBtn.getAttribute("data-room-id");
-    const chatroomPath = "/chatroom/" + roomId;
-    window.location.href = chatroomPath;
-  };
+document.getElementById("add-to-chat").addEventListener("click", addMember);
+const resultContainer = document.getElementById("result-container");
+async function addMember(event) {
+  event.preventDefault();
+  const checkbox = document.getElementById("default-checkbox");
 
-  document
-    .getElementById("close-addmember-modal")
-    .addEventListener("click", () => {
-      addMemberMessage.textContent = "";
-      resultContainer.innerHTML = "";
-      emailInput.value = "";
-      backToChatroom(document.getElementById("close-addmember-modal"));
-    });
-
-  addMemberBackToChatBtn.addEventListener("click", () => {
-    backToChatroom(addMemberBackToChatBtn);
-  });
-
-  //@ helper function to remove member
-  const removeModalResultContainer = document.getElementById(
-    "remove-result-container"
-  );
-  const removeMemberModalMessage = document.getElementById(
-    "removeMemberMessage"
-  );
-  const removeMemberModalForm = document.querySelector(".remove-member-form");
-  const removeModalBackToChatBtn = document.getElementById(
-    "remove-back-to-chat"
-  );
-
-  const removeMember = async (event, email) => {
-    event.preventDefault();
+  if (checkbox.checked) {
+    const email = checkbox.value;
     try {
-      const response = await fetch("/remove-member", {
+      const response = await fetch("/add-member", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -319,75 +258,139 @@ async function getCurrentUser() {
       const result = await response.json();
 
       if (result.success) {
-        removeMemberModalForm.classList.add("noDisplay");
-        removeModalResultContainer.classList.remove("noDisplay");
-        removeMemberModalMessage.textContent = `${result.message}`;
+        addMemberMessage.textContent = `${result.message}`;
+        addMemberMessage.classList.add("text-blue-500");
+        addMemberMessage.classList.remove("text-red-600");
+        addMemberBackToChatBtn.classList.remove("noDisplay");
+        addChatMemberBtn.classList.add("noDisplay");
       } else {
         console.log("error: ", result.error);
-        removeMemberModalMessage.classList.add("text-red-600");
-        removeMemberModalMessage.classList.remove("text-blue-600");
-        removeMemberModalMessage.textContent = `${result.error}, please try again.`;
+        addMemberMessage.textContent = `${result.error}`;
       }
     } catch (error) {
-      removeMemberModalMessage.classList.add("text-red-600");
-      removeMemberModalMessage.classList.remove("text-blue-600");
-      removeMemberModalMessage.textContent = `${error}`;
+      addMemberMessage.textContent = `${error}`;
     }
-  };
+  } else {
+    addMemberMessage.textContent = `Please confirm the user by checking the box.`;
+  }
+}
 
-  //@ highlight member in chat
-  document.querySelectorAll(".member-card").forEach((card) => {
-    card.addEventListener("click", (event) => {
-      event.stopPropagation();
-      document.querySelectorAll(".member-card").forEach((card) => {
-        card.classList.remove("active", "border-green-500");
-      });
-      card.classList.add("active", "border-green-500");
-      document.querySelectorAll(".remove-button").forEach((button) => {
-        button.classList.add("hidden");
-      });
-      card.querySelector(".remove-button").classList.remove("hidden");
+//@ helper function: go back to chatroom
 
-      //@ Show remove member modal
-      const memberEmail = card.getAttribute("data-memberEmail");
-      const memberUsername = card.getAttribute("data-memberUsername");
+const backToChatroom = (triggeredBtn) => {
+  const roomId = triggeredBtn.getAttribute("data-room-id");
+  const chatroomPath = "/chatroom/" + roomId;
+  window.location.href = chatroomPath;
+};
 
-      const removeMemberModal = document.querySelector("#removeMember-modal");
-
-      removeMemberModal.querySelector(
-        "#remove-warning-title"
-      ).innerHTML = `Remove <strong>${memberUsername} (${memberEmail})</strong>`;
-      removeMemberModal.querySelector(
-        "#remove-warning-detail"
-      ).innerHTML = `Are you sure you want to remove <strong>${memberUsername} (${memberEmail})</strong> from this chat?`;
-
-      let confirmRemoveBtn = removeMemberModal.querySelector("#confirm-remove");
-      const newConfirmRemoveBtn = confirmRemoveBtn.cloneNode(true);
-      confirmRemoveBtn.parentNode.replaceChild(
-        newConfirmRemoveBtn,
-        confirmRemoveBtn
-      );
-      confirmRemoveBtn = newConfirmRemoveBtn;
-      confirmRemoveBtn.addEventListener("click", (event) => {
-        removeMember(event, memberEmail);
-      });
-    });
+document
+  .getElementById("close-addmember-modal")
+  .addEventListener("click", () => {
+    addMemberMessage.textContent = "";
+    resultContainer.innerHTML = "";
+    emailInput.value = "";
+    backToChatroom(document.getElementById("close-addmember-modal"));
   });
 
-  document.addEventListener("click", () => {
+addMemberBackToChatBtn.addEventListener("click", () => {
+  backToChatroom(addMemberBackToChatBtn);
+});
+
+//@ helper function to remove member
+const removeModalResultContainer = document.getElementById(
+  "remove-result-container"
+);
+const removeMemberModalMessage = document.getElementById(
+  "removeMemberMessage"
+);
+const removeMemberModalForm = document.querySelector(".remove-member-form");
+const removeModalBackToChatBtn = document.getElementById(
+  "remove-back-to-chat"
+);
+
+const removeMember = async (event, email) => {
+  event.preventDefault();
+  try {
+    const response = await fetch("/remove-member", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, chatRoomId }),
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+      removeMemberModalForm.classList.add("noDisplay");
+      removeModalResultContainer.classList.remove("noDisplay");
+      removeMemberModalMessage.textContent = `${result.message}`;
+    } else {
+      console.log("error: ", result.error);
+      removeMemberModalMessage.classList.add("text-red-600");
+      removeMemberModalMessage.classList.remove("text-blue-600");
+      removeMemberModalMessage.textContent = `${result.error}, please try again.`;
+    }
+  } catch (error) {
+    removeMemberModalMessage.classList.add("text-red-600");
+    removeMemberModalMessage.classList.remove("text-blue-600");
+    removeMemberModalMessage.textContent = `${error}`;
+  }
+};
+
+//@ highlight member in chat
+document.querySelectorAll(".member-card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    event.stopPropagation();
     document.querySelectorAll(".member-card").forEach((card) => {
       card.classList.remove("active", "border-green-500");
     });
+    card.classList.add("active", "border-green-500");
     document.querySelectorAll(".remove-button").forEach((button) => {
       button.classList.add("hidden");
     });
-  });
+    card.querySelector(".remove-button").classList.remove("hidden");
 
-  removeModalBackToChatBtn.addEventListener("click", () => {
-    const roomId = removeModalBackToChatBtn.getAttribute("data-room-id");
-    const chatroomPath = "/chatroom/" + roomId;
-    window.location.href = chatroomPath;
-  });
+    //@ Show remove member modal
+    const memberEmail = card.getAttribute("data-memberEmail");
+    const memberUsername = card.getAttribute("data-memberUsername");
 
-})(); 
+    const removeMemberModal = document.querySelector("#removeMember-modal");
+
+    removeMemberModal.querySelector(
+      "#remove-warning-title"
+    ).innerHTML = `Remove <strong>${memberUsername} (${memberEmail})</strong>`;
+    removeMemberModal.querySelector(
+      "#remove-warning-detail"
+    ).innerHTML = `Are you sure you want to remove <strong>${memberUsername} (${memberEmail})</strong> from this chat?`;
+
+    let confirmRemoveBtn = removeMemberModal.querySelector("#confirm-remove");
+    const newConfirmRemoveBtn = confirmRemoveBtn.cloneNode(true);
+    confirmRemoveBtn.parentNode.replaceChild(
+      newConfirmRemoveBtn,
+      confirmRemoveBtn
+    );
+    confirmRemoveBtn = newConfirmRemoveBtn;
+    confirmRemoveBtn.addEventListener("click", (event) => {
+      removeMember(event, memberEmail);
+    });
+  });
+});
+
+document.addEventListener("click", () => {
+  document.querySelectorAll(".member-card").forEach((card) => {
+    card.classList.remove("active", "border-green-500");
+  });
+  document.querySelectorAll(".remove-button").forEach((button) => {
+    button.classList.add("hidden");
+  });
+});
+
+removeModalBackToChatBtn.addEventListener("click", () => {
+  const roomId = removeModalBackToChatBtn.getAttribute("data-room-id");
+  const chatroomPath = "/chatroom/" + roomId;
+  window.location.href = chatroomPath;
+});
+
+}) ();
 
